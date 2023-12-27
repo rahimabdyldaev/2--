2e3fc7959eb1a6f09579a10fe17dd4a746236eb6
rahimabdyldaev <149590941+rahimabdyldaev@users.noreply.github.com>
2023-12-27 19:01:44 +0600

    Add files via upload
    Шоп ласт лессон

--- src/App.js
+++ src/App.js
@@ -1,18 +1,54 @@
-import React, { useState } from "react";
-import Form from "./Form";
-import Table from "./Table";
-import './style.css'
+import {BrowserRouter, Routes, Route, json} from 'react-router-dom';
+import Home from './pages/Home/Home';
+import Cart from './pages/Cart/Cart';
+import Header from './components/Header/Header';
+import './style.css';
+import { useEffect, useState } from 'react';
+import Category from './pages/Category/Category';
+import Details from './pages/Details/Details';
 
 function App() {
-  const [data,  setData] = useState({});
+  const [cartData, setCartData] = useState(
+    localStorage.getItem('cartData')
+    ? JSON.parse(localStorage.getItem('cartData'))
+    : []
+  );
+  const buyFunc = (obj) =>{
+    const idx = cartData.findIndex(item =>{
+      return obj.id == item.id
+    });
+
+if(idx > -1){
+  cartData[idx].count = cartData[idx].count + 1;
+  setCartData([...cartData]);
+}else{
+  setCartData([{
+    ...obj,
+    count:1,
+  }, ...cartData])
+}
+  }
+
+useEffect(()=>{
+  localStorage.setItem('cartData', JSON.stringify(cartData))
+  
+},[cartData])
 
   return (
-    <div>
+    <BrowserRouter>
+      <Header cartData={cartData} />
 
-    <Form data={data} setData={setData}/>
-    <Table data={data}/>
-    
-    </div>
+      <Routes>
+      <Route path={'/'} element={<Home buyFunc={buyFunc}/>}/>
+      <Route path='/cart' element={<Cart
+      buyFunc={buyFunc} 
+      cartData={cartData} 
+      setCartData={setCartData} />}/>
+      <Route path='/category/:category' element={<Category buyFunc={buyFunc}/>}/>
+      <Route path='/details/:id' element={<Details buyFunc={buyFunc}/>} />
+      </Routes>
+
+    </BrowserRouter>
   );
 }
 
